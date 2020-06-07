e3736d91e482ffebb8f9ed2af887983ffadb3ca7
Varun Pusarla <varunpusarla@gmail.com>
2020-06-07 18:02:41 +0530

    Updates

---
+++ precache-manifest.272d9c11330a3a05655b6f2db0fb2aec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ddb379916665f1aa46fc38096aa2825",
+    "url": "/The-Weather-App/index.html"
+  },
+  {
+    "revision": "34d5151be522e0b82eb6",
+    "url": "/The-Weather-App/static/css/main.67dc9249.chunk.css"
+  },
+  {
+    "revision": "394ed6084b70c0885b03",
+    "url": "/The-Weather-App/static/js/2.217bc631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/The-Weather-App/static/js/2.217bc631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d5151be522e0b82eb6",
+    "url": "/The-Weather-App/static/js/main.2fd450f7.chunk.js"
+  },
+  {
+    "revision": "e7048caaea2cb8e71ee8",
+    "url": "/The-Weather-App/static/js/runtime-main.3744e314.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/The-Weather-App/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/The-Weather-App/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
